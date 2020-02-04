025138c8f1df9fb441cb86e29b4d32b199dc158b
Eric Davenport <EricDavenport@pursuit.org>
2020-02-04 16:14:41 -0500

    recommit - issues withnblast commit

---
+++ WeatherApp/Models/PhotoModel.swift
@@ -0,0 +1,23 @@
+//
+//  PhotoModel.swift
+//  WeatherApp
+//
+//  Created by Eric Davenport on 2/4/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import Foundation
+
+struct PhotoObject: Codable {
+  let hits: [PhotoInfo]
+}
+
+struct PhotoInfo: Codable {
+  let largeImageURL : String
+  let likes : Int
+  let views : Int
+  let tags : String
+  let previewURL : String
+  let favorites : Int
+  let webformatURL : String
+}
